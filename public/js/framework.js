a086d82d5f671971f35c3de937a93424b0d1ae9f
Lili <insa.lilitong@gmail.com>
2016-01-06 23:06:31 +0100

    add location

--- public/js/framework.js
+++ public/js/framework.js
@@ -3,16 +3,20 @@
  */
 $(document).ready(function() {
     var view = {
+        // view init, set button event
         init: function(){
             this.addLocationBtn = $('#addLocation');
             this.addLocationBtn.on('click', function(){
                 octopus.addLocation(this.parentNode);
+                octopus.renderLocation();
             });
 
             this.render();
         },
+
+        // initiate render, show all the former location data. Only be executed once
         render: function(){
-            var locations = octopus.storeLocations();
+            var locations = octopus.locations;
             for(var i=0; i<octopus.locationAmount;i++){
                 var location = locations[i];
                 var text = '<div id="location'+location.number+'">';
@@ -24,19 +28,36 @@ $(document).ready(function() {
                 text += '<button class="editBtn btn" name="edit" value='+location.number+'>Edit</button></div>';
                 $('#locations').append(text);
             }
-            $('.editBtn').on('click', function(){
+            $('#locations').on('click', '.editBtn',function(){
                 octopus.editLocation(this);
             });
+        },
+
+        // when a new location has been added, render it on the page
+        renderNew: function(){
+            var currentLocation = octopus.locations[octopus.locationAmount-1];
+            var text = '<div id="location'+currentLocation.number+'">';
+            text += '<h3>Location'+currentLocation.number+'</h3>';
+            text += 'Longitude: <input class="location-long" type="text" name="longitude" disabled="disabled" value="'+currentLocation.long+'">';
+            text += 'Latitude: <input class="location-lati" type="text" name="latitude" disabled="disabled" value="'+currentLocation.lati+'">';
+            text += 'Name: <input class="location-name" type="text" name="name" disabled="disabled" value="'+currentLocation.name+'">';
+            text += 'Data: <input class="location-data" type="text" name="data" disabled="disabled" value="'+currentLocation.data+'">';
+            text += '<button class="editBtn btn" name="edit" value='+currentLocation.number+'>Edit</button></div>';
+            $('#locations').append(text);
         }
     };
 
     var model = {
-        locations: [{},{},{}],
+
+        // model init, database init
         init: function(){
+            this.locationsDB = [];
+            this.locationAmountDB = null;
             this.db = new PouchDB('http://localhost:5984/insect');
             this.getLocations();
         },
 
+        // initiate, get former location data from database, execute once
         getLocations: function(){
             this.db.allDocs({
                 include_docs: true,
@@ -44,55 +65,68 @@ $(document).ready(function() {
                 startkey: 'location',
                 endkey: 'location\uffff'
             }).then(function(locationData){
-                model.locationAmount = locationData.rows.length;
-                for(var i = 0; i < model.locationAmount; i++){
-                    model.locations[i].name = locationData.rows[i].doc.name;
-                    model.locations[i].long = Number(locationData.rows[i].doc.long);
-                    model.locations[i].lati = Number(locationData.rows[i].doc.lati);
-                    model.locations[i].data = locationData.rows[i].doc.data;
-                    model.locations[i].number = locationData.rows[i].doc.number;
+                model.locationAmountDB = locationData.rows.length;
+                // store data into model.locations
+                for(var i = 0; i < model.locationAmountDB; i++){
+                    var currentLocation = {};
+                    currentLocation.name = locationData.rows[i].doc.name;
+                    currentLocation.long = Number(locationData.rows[i].doc.long);
+                    currentLocation.lati = Number(locationData.rows[i].doc.lati);
+                    currentLocation.data = locationData.rows[i].doc.data;
+                    currentLocation.number = locationData.rows[i].doc.number;
+                    model.locationsDB.push(currentLocation);
                 }
-                if(model.locationAmount>=1){
-                    $('#addLocation').val(model.locations[i-1].number+1);
-                }else{
-                    $('#addLocation').val(1);
-                }
+
+                //octopus init
                 octopus.init();
             });
         },
 
+        // put data into database
         upload: function(){
-            var id = "location"+octopus.locationNumber;
+            var currentLocation = octopus.locations[octopus.locationAmount-1];
+            var id = "location"+currentLocation.number;
             this.db.upsert(id, function(doc){
                 return{
-                    "number": octopus.locationNumber,
-                    "name": octopus.locationName,
-                    "long": octopus.locationLong,
-                    "lati": octopus.locationLati,
-                    "data": octopus.locationData
+                    "number": currentLocation.number,
+                    "name": currentLocation.name,
+                    "long": currentLocation.long,
+                    "lati": currentLocation.lati,
+                    "data": currentLocation.data
                 }
             });
         }
     };
 
+    // connection between view and model
     var octopus = {
         init: function(){
+            this.locations = model.locationsDB;
+            this.locationAmount = model.locationAmountDB;
             view.init();
+            this.updateBtnVal();
         },
 
-        storeLocations: function(){
-            octopus.locationAmount = model.locationAmount;
-            return model.locations;
+        // set addLocation button value. If there are former location, button value=number(last)+1. If not, button value=1
+        updateBtnVal: function(){
+            var amount = this.locationAmount;
+            if(amount>=1){
+                $('#addLocation').val(this.locations[amount-1].number+1);
+            }else{
+                $('#addLocation').val(1);
+            }
         },
 
         addLocation: function(location){
-            this.locationName = $(location).find('.location-name').val();
-            this.locationLong = $(location).find('.location-long').val();
-            this.locationLati = $(location).find('.location-lati').val();
-            this.locationData = $(location).find('.location-data').val();
-            this.locationNumber = Number($(location).find('button').val());
+            var currentLocation = {};
+            currentLocation.name = $(location).find('.location-name').val();
+            currentLocation.long = $(location).find('.location-long').val();
+            currentLocation.lati = $(location).find('.location-lati').val();
+            currentLocation.data = $(location).find('.location-data').val();
+            currentLocation.number = Number($(location).find('button').val());
+            this.locations.push(currentLocation);
+            this.locationAmount++;
             model.upload();
-            view.render();
         },
 
         editLocation: function(button){
@@ -109,6 +143,11 @@ $(document).ready(function() {
                 this.addLocation(current);
             }
 
+        },
+
+        renderLocation: function(){
+            view.renderNew();
+            this.updateBtnVal();
         }
     };
 
