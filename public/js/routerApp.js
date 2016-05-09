8d93448bdf03a2c66dd7ea3daed7a269060b2d4f
Lili <insa.lilitong@gmail.com>
2016-05-09 18:11:21 +0200

    teacher page

--- public/js/routerApp.js
+++ public/js/routerApp.js
@@ -1,4 +1,4 @@
-var routerApp = angular.module('routerApp', ['ui.router', 'uiRouterStyles','AppSetModule', 'MapModule','PlayerModule']);
+var routerApp = angular.module('routerApp', ['ui.router', 'uiRouterStyles','AppSetModule', 'MapModule','PlayerModule', 'TeacherModule']);
 
 /**
  * 由于整个应用都会和路由打交道，所以这里把$state和$stateParams这两个对象放到$rootScope上，方便其它地方引用和注入。
--- public/js/teachermodule.js
+++ public/js/teachermodule.js
@@ -1,7 +1,34 @@
 var teacherModule = angular.module("TeacherModule", []);
 
-teacherModule.controller('TeacherCtrl', function($scope, DataService,$timeout){
+teacherModule.controller('TeacherCtrl', function($scope,$timeout){
 	var socket = io.connect('http://localhost:8000');
-    // var socket = io.connect('https://myoa.herokuapp.com');
+	$scope.range = function(n) {
+		var num = parseInt(n);
+    return new Array(num);   
+  }
+  // var socket = io.connect('https://myoa.herokuapp.com');
+
+  getDeploy = function(){
+    var db = new PouchDB('https://myoa.smileupps.com/user');
+    db.get('setting').then(function (docs) {
+      // handle result
+      // store deployment
+      $scope.deploy = docs.deploy;
+      $scope.currentClass = docs.classroom[docs.deploy.classroom];
+      $scope.$apply();
+      // get app based on the deploy
+      var apps = new PouchDB('https://myoa.smileupps.com/myoa');
+      apps.get($scope.deploy.app).then(function(doc){
+          $scope.markers = doc.mapstep1.markers;
+          $scope.$apply();
+      }).catch(function (err) {
+          console.log(err);
+      });
+    }).catch(function (err) {
+      console.log(err);
+    }); 
+  }
+
+  getDeploy();
 
 });
