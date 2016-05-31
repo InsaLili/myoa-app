df9a9b2c1826c88165d7b1f04b38395738d164af
Lili <insa.lilitong@gmail.com>
2016-06-01 00:17:33 +0200

    hide error

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 // development error handler
 // will print stacktrace
